50c35d6f2a849f4f9d433b365614ca81f7f920f4
Maria <mmorrison1978@gmail.com>
2016-02-18 16:14:30 -0700

    test data is showing up

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function(){
+  // var app = angular.module('clone', [ ]);
   var app = angular.module('clone', []);
 
   app.controller('CloneController',function() {
@@ -11,10 +12,19 @@
       title: 'Test Title',
       votes: 10,
       author: 'Maria Morrison',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.',
-      data: '2/8/2016',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      date: '2/8/2016',
       commentsCount: 2,
+    },
+    {
+      imgUrl: 'http://vignette2.wikia.nocookie.net/zimwiki/images/7/7b/Art_Zimdisguisepointing.png/revision/latest?cb=20100920105522',
+      title: 'Test Title Number Two',
+      votes: 0,
+      author: 'Bernie Sanders',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      date: '1/1/2016',
+      commentsCount: 7,
     }
   ];
 
-})
+})();
