cd36b1d0206fc955d72b3cec87497270818e273f
Maria <mmorrison1978@gmail.com>
2016-02-23 10:55:21 -0700

    added animate and color

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 (function(){
-  var app = angular.module('clone', []);
+  var app = angular.module('clone', ["ngAnimate"]);
+  // $scope.sort = [{Sort By Votes}, {Sort By Date}, {Sort By Title}];
 
   app.controller('CloneController', function($scope){
+    $scope.addPost = function(){
+      newPost = {};
+      newPost.title = $scope.title;
+      newPost.author = $scope.author;
+      newPost.imgUrl = $scope.imgUrl;
+      newPost.comments = [];
+      newPost.commentsCount = 0;
+      newPost.description = $scope.description;
+      newPost.createdOn = Date.now();
+      $scope.AllPosts.push(newPost);
+      $scope.title = null;
+      $scope.author = null;
+      $scope.imgUrl = null;
+      $scope.description = null;
+    };
 
-      $scope.addPost = function(){
-        newPost = {};
-        newPost.title = $scope.title;
-        newPost.author = $scope.author;
-        newPost.imgUrl = $scope.imgUrl;
-        newPost.comments = [];
-        newPost.commentsCount = 0;
-        newPost.description = $scope.description;
-        newPost.createdOn = Date.now();
-        $scope.AllPosts.push(newPost);
-        $scope.title = null;
-        $scope.author = null;
-        $scope.imgUrl = null;
-        $scope.description = null;
-      };
-
-      $scope.addComment = function(x){
-        newComment = {};
-        newComment.name = x.name;
-        newComment.body = x.body;
-        x.post.comments.push(newComment);
-        x.post.commentsCount++;
-        x.name = null;
-        x.body = null;
-      };
+    $scope.addComment = function(x){
+      newComment = {};
+      newComment.name = x.name;
+      newComment.body = x.body;
+      x.post.comments.push(newComment);
+      x.post.commentsCount++;
+      x.name = null;
+      x.body = null;
+    };
 
 
 
@@ -53,7 +53,7 @@
     {
     imgUrl: 'http://vignette2.wikia.nocookie.net/zimwiki/images/7/7b/Art_Zimdisguisepointing.png/revision/latest?cb=20100920105522',
     title: ' A Test Title Number Two',
-    votes: 0,
+    votes: 1,
     author: 'Bernie Sanders',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     createdOn: 1455908986000,
@@ -65,6 +65,30 @@
       },
       {
         name: 'Buddy Rich',
+        body: 'Lorem ipsum dolor sit amet, consectetur'
+      },
+    ],
+
+    },
+    {
+    imgUrl: 'http://images5.fanpop.com/image/photos/28300000/Gir-3-gir-28304244-294-399.png',
+    title: 'Be Good',
+    votes: 3,
+    author: 'Cathy Collier',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    createdOn: 1455908986200,
+    commentsCount: 3,
+    comments: [
+      {
+        name: 'Abbie Hoffman',
+        body: 'Lorem ipsum dolor sit amet, consectetur'
+      },
+      {
+        name: 'Buddy Rich',
+        body: 'Lorem ipsum dolor sit amet, consectetur'
+      },
+      {
+        name: 'Sterling Archer',
         body: 'Lorem ipsum dolor sit amet, consectetur'
       },
     ],
