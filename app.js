18ec47838963968751474e7e1202181cc5afac8d
Maria <mmorrison1978@gmail.com>
2016-02-22 20:21:11 -0700

    pluralize working

--- app.js
+++ app.js
@@ -36,7 +36,7 @@
     {
     imgUrl: 'http://vignette3.wikia.nocookie.net/bleedmancomics/images/8/8c/Girtrans.png/revision/latest?cb=20121012154620',
     title: 'Test Title',
-    votes: 10,
+    votes: 0,
     author: 'Maria Morrison',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     createdOn: 1455908986000,
@@ -52,8 +52,8 @@
 
     {
     imgUrl: 'http://vignette2.wikia.nocookie.net/zimwiki/images/7/7b/Art_Zimdisguisepointing.png/revision/latest?cb=20100920105522',
-    title: 'Test Title Number Two',
-    votes: 100,
+    title: ' A Test Title Number Two',
+    votes: 0,
     author: 'Bernie Sanders',
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     createdOn: 1455908986000,
